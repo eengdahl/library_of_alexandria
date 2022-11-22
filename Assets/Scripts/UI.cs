4b5df9dcf485e81bfc3321f003ede2206cb70839
eengdahl <engdahl.emil@gmail.com>
2022-11-22 13:37:14 +0100

    Testing noisemeter

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -10,24 +10,31 @@ public class UI : MonoBehaviour
     public Slider showTimeLeft;
     public Slider noiseMeter;
     public float timer = 300f;
+    float noNoise = 5f;
 
 
     // Update is called once per frame
     void Update()
     {
+        //Creating a timer and counting down from full time to no time 
         timer = timer - Time.deltaTime;
         showTimeLeft.value = timer;
 
+        //testing so the noisemeter Reacts
         if (Input.GetButtonDown("Jump"))
         {
             Debug.Log("Ping");
-            UpdateNoise(10);
+            UpdateNoise(1);
         }
     }
 
 
+    //Function for very basic update of noisemeter
     public void UpdateNoise(int noise)
     {
+      
+
+
         noiseMeter.value += noise;
 
         if (true)
