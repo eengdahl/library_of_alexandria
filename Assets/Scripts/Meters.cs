ccc9a53ead17919ed964f2c689e156c08e0af1d6
eengdahl <engdahl.emil@gmail.com>
2022-11-22 13:51:23 +0100

    Test fixing merge prob meters/ui

---
+++ Assets/Scripts/Meters.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Meters : MonoBehaviour
+{
+    public Slider showTimeLeft;
+    public Slider noiseMeter;
+    public float timer = 300f;
+    float noNoise = 5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Creating a timer and counting down from full time to no time 
+        timer = timer - Time.deltaTime;
+        showTimeLeft.value = timer;
+
+        //testing so the noisemeter Reacts
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Ping");
+            UpdateNoise(1);
+        }
+    }
+
+
+    //Function for very basic update of noisemeter
+    public void UpdateNoise(int noise)
+    {
+      
+
+
+        noiseMeter.value += noise;
+
+        if (true)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("scene_main");
+    }
+}
