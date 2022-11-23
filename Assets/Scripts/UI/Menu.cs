8296bbfbaccf562e3bba56f93fe1cfea44049ace
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2022-11-23 11:45:36 +0100

    Simple Main Menu

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -7,8 +7,9 @@ using TMPro;
 
 public class Menu : MonoBehaviour
 {
-
-
+    public GameObject ButtonPanelOnOff;
+    public GameObject CreditsOnOff;
+    public GameObject SettingsOnOff;
     public void Quit()
     {
 #if UNITY_EDITOR
@@ -21,5 +22,29 @@ public class Menu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("scene_main");
+    }
+
+    public void Credits()
+    {
+        ButtonPanelOnOff.SetActive(false);
+        CreditsOnOff.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        ButtonPanelOnOff.SetActive(true);
+        CreditsOnOff.SetActive(false);
+    }
+
+    public void Settings()
+    {
+        ButtonPanelOnOff.SetActive(false);
+        SettingsOnOff.SetActive(true);
+    }
+
+    public void Settings_to_MainMenu()
+    {
+        ButtonPanelOnOff.SetActive(true);
+        SettingsOnOff.SetActive(false);
     }
 }
