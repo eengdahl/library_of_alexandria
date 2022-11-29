70e0048b760a87dd876c141c05c76e28f042a24c
eengdahl <engdahl.emil@gmail.com>
2022-11-29 10:28:15 +0100

    Changed spawnplace for registratade books

--- Assets/Scripts/Player/Registration.cs
+++ Assets/Scripts/Player/Registration.cs
@@ -8,25 +8,31 @@ public class Registration : MonoBehaviour
     public float timer;
     private float spawnrate;
     public Rigidbody2D rb;
+    private Vector2 spawnPosition;
 
 
     // Start is called before the first frame update
     void Start()
     {
         spawnrate = 1;
+
+        //Hardcoded spawnposition for registrated books
+        spawnPosition = new Vector2(7.1f, -1.8f);
     }
 
 
     public void OnTriggerStay2D(Collider2D collision)
     {
+        //If player holds left shift books are beeing registrated and put on the table
         if (collision.tag == "Registration" && Input.GetKey(KeyCode.LeftShift))
         {
 
             timer += Time.deltaTime;
             if (timer > spawnrate)
             {
-                 Instantiate(books[Random.Range(0, books.Count)], rb.position, Quaternion.identity);
-            
+
+                Instantiate(books[Random.Range(0, books.Count)], spawnPosition, Quaternion.identity);
+
                 timer = 0;
             }
         }
