f31a3514be46dd2f1c94228d045a883d5fb4f0f9
eengdahl <engdahl.emil@gmail.com>
2022-12-05 12:53:36 +0100

    Change NPC-husheble zone AND playing with karins hush

--- Assets/Scripts/Player/MakeHuschSound.cs
+++ Assets/Scripts/Player/MakeHuschSound.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class MakeHuschSound : MonoBehaviour
 {
     public AudioClip[] schhSound;
+    public AudioClip longHush;
     AudioSource audioSource;
     float huschTimer;
     public float lenghtOfHusch;
     public bool doesHuschSound;
     Animator animatorKarin;
+    private float chargedHush;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         animatorKarin = GetComponentInParent<Animator>();
-       // Random.Range(0, schhSound.Length);
+        // Random.Range(0, schhSound.Length);
         audioSource.clip = schhSound[Random.Range(0, schhSound.Length)];
     }
 
@@ -24,19 +26,37 @@ public class MakeHuschSound : MonoBehaviour
     void Update()
     {
         //Om du trycker på  activate spela husch ljudet och gör om boolen doesHuschSound till true
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKey("space"))
+        {
+            chargedHush += Time.deltaTime;
+
+        }
+        //Makes a chargedHush
+        if (Input.GetKeyUp("space") && chargedHush > 0.5f)
+        {
+            doesHuschSound = true;
+            audioSource.clip = longHush;
+            Invoke("StopAudio",chargedHush);
+            audioSource.Play();
+            animatorKarin.SetBool("IsHushing", true);
+            chargedHush = 0;
+        }
+        //Makes standard hush
+        else if (Input.GetKeyUp("space"))
         {
             doesHuschSound = true;
             audioSource.clip = schhSound[Random.Range(0, schhSound.Length)];
             audioSource.Play();
             animatorKarin.SetBool("IsHushing", true);
+
+            chargedHush = 0;
         }
         //Om vi huschar håll boolen igång i längden "lengthOfHusch" sec stäng sedan av husch boolen 
         if (doesHuschSound)
         {
             huschTimer += Time.deltaTime;
 
-         
+
             if (huschTimer >= lenghtOfHusch)
             {
                 animatorKarin.SetBool("IsHushing", false);
@@ -50,5 +70,10 @@ public class MakeHuschSound : MonoBehaviour
             animatorKarin.SetBool("IsHushing", false);
             huschTimer = 0;
         }
+    }
+
+    private void StopAudio()
+    {
+        audioSource.Stop();
     }
 }
