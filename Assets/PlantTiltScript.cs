9a78b91d30d62ff98dfed0d6a5d16cfce9c1e33a
JovinFast <112895166+JovinFast@users.noreply.github.com>
2022-12-10 02:37:24 +0100

    Plants move when walked into

---
+++ Assets/PlantTiltScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantTiltScript : MonoBehaviour
+{
+    
+    float tiltSpeed = 10f;
+    float angle;
+    float targetAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAngle = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, tiltSpeed * Time.deltaTime);
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if(collision.transform.position.x > transform.position.x)
+            {
+                targetAngle = 4;
+                //transform.eulerAngles = new Vector3(0, 0, angle);
+            }
+            if (collision.transform.position.x < transform.position.x)
+            {
+                targetAngle = -4;
+                //transform.eulerAngles = new Vector3(0, 0, angle);
+            }
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        targetAngle = 0;
+        //transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+}
