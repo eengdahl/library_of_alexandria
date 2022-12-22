0d48c92a34c22d020b0390aa967ab6bb51c9bcb5
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2022-12-22 13:30:26 +0100

    tutorial switch page

--- Assets/Tutorial_book_swicher.cs
+++ Assets/Tutorial_book_swicher.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class Tutorial_book_swicher : MonoBehaviour
 {
-    int i = 0;
+    public int i = 1;
     public GameObject [] books;
     public GameObject start_continue_button;
+     Animator m_Animator;
+
+    void Start() 
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+         if (Input.GetKeyDown("space"))
         {
             books[i].SetActive(true);
-            i += 1;
+            i=i +2;
+
         }
-        if (i == books.Length-1)
+        if (i >= books.Length-1)
         {
             start_continue_button.SetActive(true);
-        }
+        } 
+        
+    }
+
+    void disableanimation()
+    {
+        i ++;
+        m_Animator.enabled=false;
+        books[i].SetActive(true);
+        Debug.Log(i);
+    }
+    void nextbook()
+    {
+        //books[i].SetActive(true);
     }
 }
