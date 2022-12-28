f7cb1aabdcd372f927dd9a071371260657e3060c
JovinFast <112895166+JovinFast@users.noreply.github.com>
2022-12-28 14:37:48 +0100

    Title Screen

---
+++ Assets/TimerForTItle.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class TimerForTItle : MonoBehaviour
+{
+    public SpriteRenderer continueButton;
+    public TMP_Text continueText;
+    public float timer;
+    public AudioClip huschSound;
+    AudioSource aS;
+    void Start()
+    {
+        aS = GetComponent<AudioSource>();
+        continueButton.enabled = false;
+        continueText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            continueButton.enabled = true;
+            continueText.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            aS.PlayOneShot(huschSound);
+            Invoke("LoadMenu", huschSound.length);
+            
+        }
+
+    }
+    void LoadMenu()
+    {
+        SceneManager.LoadScene("Meny");
+    }
+}
